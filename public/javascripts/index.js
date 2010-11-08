b7b4bbd8f16175f0d67e70db8c641b05349877fc
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-08 16:47:39 +0000

    Add 'interact' event, and make panel listen for it.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,43 +5,9 @@ Ext.setup({
   onReady: function() {
     var eventPanel, viewport;
 
-    // this doesn't work!
-    // eventPanel = new Ext.Panel({
-    //   listeners: {
-    //     tap: function() {console.log('tapped on body');},
-    //     scope: this
-    //   }
-    // });
-
-    // To access DOM events directly from a Component's HTMLElement, listeners must
-    // be added to the Element after the Component has been rendered
-    // eventPanel = new Ext.Panel({
-    //   listeners: {
-    //     el: {
-    //       tap: function() {console.log('tapped on element');}
-    //     },
-    //     scope: this
-    //   }
-    // });
-
-    // You can also add events after creating an object, 2 ways.
-    // passing 2-4 arguments:
-    // eventPanel = new Ext.Panel();
-    // eventPanel.addListener(
-    //   'tap',                                          // eventName
-    //   function() {console.log('tapped on body');},    // handler
-    //   this,                                           // scope
-    //   { element: 'body'}                              // options
-    // );
-
-    // passing a single configuration object:
-    // allows for multiple listeners to be created at once
-    eventPanel = new Ext.Panel();
+    eventPanel = new InteractivePanel();
     eventPanel.addListener({
-      body: {
-        tap: function() {console.log('tapped on body');},
-        swipe: function() {console.log('swiped body');}
-      }
+      interact: function() {console.log('interacted with panel');}
     });
 
     viewport = new Ext.Panel({
