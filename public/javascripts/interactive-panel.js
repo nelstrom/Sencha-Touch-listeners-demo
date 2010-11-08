031894a96bf19ebaf852059e12026588896a4146
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-08 16:54:51 +0000

    Make lots of events fire 'interact' event.

--- public/javascripts/interactive-panel.js
+++ public/javascripts/interactive-panel.js
@@ -8,9 +8,23 @@ var InteractivePanel = Ext.extend( Ext.Panel, {
   afterRender: function() {
     InteractivePanel.superclass.afterRender.call(this);
     this.addManagedListener(this.el, {
-      tap: function(e) { this.fireEvent('interact', e.type, e); },
+      touchstart: this.handleEvent,
+      touchend: this.handleEvent,
+      touchmove: this.handleEvent,
+      touchdown: this.handleEvent,
+      scrollstart: this.handleEvent,
+      scroll: this.handleEvent,
+      scrollend: this.handleEvent,
+      singletap: this.handleEvent,
+      tap: this.handleEvent,
+      doubletap: this.handleEvent,
+      taphold: this.handleEvent,
+      tapcancel: this.handleEvent,
+      swipe: this.handleEvent,
       scope: this
     });
-  }
+  },
+
+  handleEvent: function(e) { this.fireEvent('interact', e.type, e); }
 
 });
