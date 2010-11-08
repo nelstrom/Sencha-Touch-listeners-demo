d71e55cdb55a6d05b865f2bfc635ce4f7d5d27fd
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-08 17:16:30 +0000

    Removed unwanted comment.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,3 @@
-// http://www.sencha.com/forum/showthread.php?105114-COMPLETE-198-Inline-method-for-attaching-dom-events&p=509711#post509711
-//
-
 Ext.setup({
   onReady: function() {
     var eventPanel, viewport;
