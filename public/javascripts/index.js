aff73cc9384216bdf5addf2762ed647209d429c9
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-08 16:34:26 +0000

    Add simple logging to panel, using 3 methods.
    * at initialization, using listeners = {} property
    * after creation, calling addListener() with a single argument
    * after creation, calling addListener() with multiple arguments

---
+++ public/javascripts/index.js
@@ -0,0 +1,54 @@
+// http://www.sencha.com/forum/showthread.php?105114-COMPLETE-198-Inline-method-for-attaching-dom-events&p=509711#post509711
+//
+
+Ext.setup({
+  onReady: function() {
+    var eventPanel, viewport;
+
+    // this doesn't work!
+    // eventPanel = new Ext.Panel({
+    //   listeners: {
+    //     tap: function() {console.log('tapped on body');},
+    //     scope: this
+    //   }
+    // });
+
+    // To access DOM events directly from a Component's HTMLElement, listeners must
+    // be added to the Element after the Component has been rendered
+    // eventPanel = new Ext.Panel({
+    //   listeners: {
+    //     el: {
+    //       tap: function() {console.log('tapped on element');}
+    //     },
+    //     scope: this
+    //   }
+    // });
+
+    // You can also add events after creating an object, 2 ways.
+    // passing 2-4 arguments:
+    // eventPanel = new Ext.Panel();
+    // eventPanel.addListener(
+    //   'tap',                                          // eventName
+    //   function() {console.log('tapped on body');},    // handler
+    //   this,                                           // scope
+    //   { element: 'body'}                              // options
+    // );
+
+    // passing a single configuration object:
+    // allows for multiple listeners to be created at once
+    eventPanel = new Ext.Panel();
+    eventPanel.addListener({
+      body: {
+        tap: function() {console.log('tapped on body');},
+        swipe: function() {console.log('swiped body');}
+      }
+    });
+
+    viewport = new Ext.Panel({
+      fullscreen: true,
+      layout: "fit",
+      items: [eventPanel]
+    });
+
+  }
+});
