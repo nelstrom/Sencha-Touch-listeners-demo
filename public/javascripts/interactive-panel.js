e67c1ae4eae0d6a84d8b255d941772fac97076cc
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-22 15:32:48 +0000

    Add listener during initialization, not after render.
    Calling addManagedListener() in the afterRender callback is no longer
    necessary. You can call add listener on the element or body of a panel
    during initialization, and it will call addManagedListener() behind the
    scenes when it is ready.

--- public/javascripts/interactive-panel.js
+++ public/javascripts/interactive-panel.js
@@ -3,25 +3,23 @@ var InteractivePanel = Ext.extend( Ext.Panel, {
   initComponent : function() {
     InteractivePanel.superclass.initComponent.call(this);
     this.addEvents('interact');
-  },
-
-  afterRender: function() {
-    InteractivePanel.superclass.afterRender.call(this);
-    this.addManagedListener(this.el, {
-      touchstart: this.handleEvent,
-      touchend: this.handleEvent,
-      touchmove: this.handleEvent,
-      touchdown: this.handleEvent,
-      scrollstart: this.handleEvent,
-      scroll: this.handleEvent,
-      scrollend: this.handleEvent,
-      singletap: this.handleEvent,
-      tap: this.handleEvent,
-      doubletap: this.handleEvent,
-      taphold: this.handleEvent,
-      tapcancel: this.handleEvent,
-      swipe: this.handleEvent,
-      scope: this
+    this.addListener({
+      el: {
+        touchstart: this.handleEvent,
+        touchend: this.handleEvent,
+        touchmove: this.handleEvent,
+        touchdown: this.handleEvent,
+        scrollstart: this.handleEvent,
+        scroll: this.handleEvent,
+        scrollend: this.handleEvent,
+        singletap: this.handleEvent,
+        tap: this.handleEvent,
+        doubletap: this.handleEvent,
+        taphold: this.handleEvent,
+        tapcancel: this.handleEvent,
+        swipe: this.handleEvent,
+        scope: this
+      }
     });
   },
 
